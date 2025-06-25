8a9704f7952790368a5ee481a36f9c988c2dad75
Oleksii Lavka <olavka@splitdev.com>
2025-06-25 20:28:23 +0300

    add customer log

--- src/controllers/webhooks/customer-create-or-update.controller.ts
+++ src/controllers/webhooks/customer-create-or-update.controller.ts
@@ -4,6 +4,7 @@ import { Request, Response } from 'express'
 
 export async function customerCreateOrUpdate(req: Request, res: Response) {
   const customer = req.body as Customer
+  console.log({ customer })
   const upsertedCustomer = await klicktippService.syncCustomer(customer)
   return res.json(upsertedCustomer)
 }
