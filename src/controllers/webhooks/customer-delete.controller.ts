cadbf31a2217a7b295038151fe250ae63780698a
Oleksii Lavka <olavka@splitdev.com>
2025-06-25 20:57:22 +0300

    added customer delete log

--- src/controllers/webhooks/customer-delete.controller.ts
+++ src/controllers/webhooks/customer-delete.controller.ts
@@ -2,8 +2,9 @@ import { getCustomerEmail, klicktippService } from '@services'
 import { Request, Response } from 'express'
 
 export async function customerDelete(req: Request, res: Response) {
-  const { admin_graphql_api_id } = req.body as { admin_graphql_api_id: string }
-  const customerEmail = await getCustomerEmail(admin_graphql_api_id)
+  const customerDeleteData = req.body as { admin_graphql_api_id: string }
+  console.log({ customerDeleteData })
+  const customerEmail = await getCustomerEmail(customerDeleteData.admin_graphql_api_id)
 
   if (customerEmail) {
     await klicktippService.removeSubscriber(customerEmail)
